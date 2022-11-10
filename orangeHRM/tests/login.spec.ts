30f30aeff8599248121feca43f9e721a0c5d57de
Azad Husen <azad.husen@visioncraft.ee>
2022-11-10 16:16:45 +0200

    orangeHRM - Login flow finished

---
+++ orangeHRM/tests/login.spec.ts
@@ -0,0 +1,42 @@
+import {test, expect, Page, chromium, Browser, BrowserContext} from "@playwright/test"
+import {LoginPage} from "../pages/login.page"
+import {data} from "../helpers/data";
+
+test.describe("Login Test", async ()=>{
+    let page: Page;
+    let browser: Browser;
+    let context: BrowserContext;
+    let login: LoginPage;
+
+    test.beforeAll(async ()=>{
+        browser = await chromium.launch();
+        context = await browser.newContext();
+        page = await context.newPage();
+        await page.goto(data.BASE_URL);
+    })
+
+    test.afterAll(async ()=>{
+        await context.close();
+        await browser.close();
+    })
+
+
+    test("Login to OrangeHRM", async ()=> {
+        login = new LoginPage(page);
+        await expect(page).toHaveURL(/.*login/);
+        await page.waitForLoadState("networkidle");
+        await login.enterLoginCredentials(data.ADMIN_USER_NAME, data.ADMIN_USER_PASSWORD)
+    });
+
+    test("Logout from the OrangeHRM", async()=>{
+        await page.waitForTimeout(3000);
+        await page.locator(".oxd-userdropdown-name").click()
+        await Promise.all([
+            page.waitForNavigation(),
+            page.locator("text=Logout").click()
+
+        ])
+    });
+
+})
+
